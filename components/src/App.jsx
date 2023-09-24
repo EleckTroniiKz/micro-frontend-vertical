cf0a9a50f99ef235bdcee78739e0c7233fa21207
ucc <ucc@eschbach.com>
2023-09-24 19:11:19 +0200

    delete components folder -> not needed

--- components/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "remixicon/fonts/remixicon.css";
-import "./index.scss";
-
-import PageRootLayout from "home/PageRootLayout";
-
-ReactDOM.render(<PageRootLayout />, document.getElementById("app"));
