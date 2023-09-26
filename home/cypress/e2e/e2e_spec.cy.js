644a801e186f6b1d5ef430fea0f79c8b54bd133f
ucc <ucc@eschbach.com>
2023-09-26 16:22:35 +0200

    Add another Test case to the e2e Test

--- home/cypress/e2e/e2e_spec.cy.js
+++ home/cypress/e2e/e2e_spec.cy.js
@@ -18,4 +18,30 @@ describe("E2E Tests for whole application", () => {
     cy.contains("TestCountry").should("be.visible");
     cy.contains("TestHeight").should("be.visible");
   });
+
+  it("removes User from List after clicking on delete", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("#addUser").click();
+    cy.get(".flex:nth-child(1) > .w-full > .w-full").click();
+    cy.get(".flex:nth-child(1) > .w-full > .w-full").type("TestNameDeletion");
+    cy.get(".flex:nth-child(2) > .w-full > .w-full").click();
+    cy.get(".flex:nth-child(2) > .w-full > .w-full").type("TestGenderDeletion");
+    cy.get(".flex:nth-child(3) > .w-full > .w-full").click();
+    cy.get(".flex:nth-child(3) > .w-full > .w-full").type(
+      "TestCountryDeletion"
+    );
+    cy.get(".flex:nth-child(4) > .w-full > .w-full").click();
+    cy.get(".flex:nth-child(4) > .w-full > .w-full").type("TestHeightDeletion");
+    cy.get("#saveUser").click();
+    cy.get(".flex-grow > a:nth-child(1)").click();
+    cy.reload();
+    cy.contains("TestNameDeletion").should("be.visible");
+
+    // now delete that user
+
+    const parent = cy.contains("td", "TestNameDeletion").parent();
+    const deleteButton = parent.find("button").contains("Delete");
+    deleteButton.click();
+    cy.contains("td", "TestNameDeletion").should("not.exist");
+  });
 });
